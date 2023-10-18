5042cec3f8f6fd98a71b01f83186898916fdb75f
Linicker <olinicker@gmail.com>
2023-10-18 12:11:07 -0300

    c0 - Início da aplicação

---
+++ Calculadora de Juros de Financiamento/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Calculadora_de_Juros_de_Financiamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Candara", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(232, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(495, 36);
+            label1.TabIndex = 0;
+            label1.Text = "Calculadora de Juros de Financiamento";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(456, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GradientActiveCaption;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 450);
+            panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(406, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(166, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Valor Principal do Empréstimo";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Panel panel1;
+        private Label label2;
+    }
+}
---
+++ Calculadora de Juros de Financiamento/Form1.cs
@@ -0,0 +1,10 @@
+namespace Calculadora_de_Juros_de_Financiamento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
